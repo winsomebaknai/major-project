dfbca7df1cf3e70a3b44367ab374a873056f4ab4
winterson <u19cse1028@cit.ac.in>
2023-02-22 04:01:23 +0530

    Init

---
+++ CustomModel.py
@@ -0,0 +1,5 @@
+from fairseq.models.transformer_lm import TransformerLanguageModel
+custom_lm = TransformerLanguageModel.from_pretrained('./checkpoints/Test_5KModel', 'checkpoint_best.pt', tokenizer='moses', bpe='fastbpe', bpe_codes="./checkpoints/Test_5KModel/codes")
+custom_lm.cuda()
+result = custom_lm.sample('', beam=5)
+print(result)
