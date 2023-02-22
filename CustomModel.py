775d7d0720e6fe90cbfe6a40eee1137f41b82284
winsome_baknai <54034787+winsomebaknai@users.noreply.github.com>
2023-02-22 15:33:11 +0530

    Update CustomModel.py

--- CustomModel.py
+++ CustomModel.py
@@ -8,22 +8,18 @@ file1.write(f"\n{result}")
 print(result)
 
 result = custom_lm.sample('माबा', beam=5)
-file1 = open('./external_files/output.txt','a')
 file1.write(f"\n{result}")
 print(result)
 
 result = custom_lm.sample('जोबोर', beam=5)
-file1 = open('./external_files/output.txt','a')
 file1.write(f"\n{result}")
 print(result)
 
 result = custom_lm.sample('मानसि', beam=5)
-file1 = open('./external_files/output.txt','a')
 file1.write(f"\n{result}")
 print(result)
 
 result = custom_lm.sample('आच्छा', beam=5)
-file1 = open('./external_files/output.txt','a')
 file1.write(f"\n{result}")
 print(result)
 
