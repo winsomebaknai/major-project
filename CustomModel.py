2edc2ef9eb60ce2a2a2492da65973ac389e3c908
winsome_baknai <54034787+winsomebaknai@users.noreply.github.com>
2023-02-22 15:26:07 +0530

    Update CustomModel.py

--- CustomModel.py
+++ CustomModel.py
@@ -1,7 +1,30 @@
 from fairseq.models.transformer_lm import TransformerLanguageModel
 custom_lm = TransformerLanguageModel.from_pretrained('./checkpoints/Test_5KModel', 'checkpoint_best.pt', tokenizer='moses', bpe='fastbpe', bpe_codes="./checkpoints/Test_5KModel/codes")
 custom_lm.cuda()
-result = custom_lm.sample('दा मोनायाव', beam=5)
-file1 = open('./external_files/output.txt','w')
+
+result = custom_lm.sample('हाबाब आदा', beam=5)
+file1 = open('./external_files/output.txt','a')
 file1.write(result)
 print(result)
+
+result = custom_lm.sample('माबा माबा', beam=5)
+file1 = open('./external_files/output.txt','a')
+file1.write(f" ${result}")
+print(result)
+
+result = custom_lm.sample('जोबोर', beam=5)
+file1 = open('./external_files/output.txt','a')
+file1.write(f" ${result}")
+print(result)
+
+result = custom_lm.sample('मानसि', beam=5)
+file1 = open('./external_files/output.txt','a')
+file1.write(f" ${result}")
+print(result)
+
+result = custom_lm.sample('आच्छा', beam=5)
+file1 = open('./external_files/output.txt','a')
+file1.write(f" ${result}")
+print(result)
+
+file1.close()
