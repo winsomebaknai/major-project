cc5c17c2d92decced5d461fbf308525101420489
winsome_baknai <54034787+winsomebaknai@users.noreply.github.com>
2023-02-22 15:15:24 +0530

    Update CustomModel.py

--- CustomModel.py
+++ CustomModel.py
@@ -1,7 +1,7 @@
 from fairseq.models.transformer_lm import TransformerLanguageModel
 custom_lm = TransformerLanguageModel.from_pretrained('./checkpoints/Test_5KModel', 'checkpoint_best.pt', tokenizer='moses', bpe='fastbpe', bpe_codes="./checkpoints/Test_5KModel/codes")
 custom_lm.cuda()
-result = custom_lm.sample('आरो', beam=5)
+result = custom_lm.sample('दा मोनायाव', beam=5)
 file1 = open('./external_files/output.txt','w')
 file1.write(result)
 print(result)
